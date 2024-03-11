1c4d367d2ac08ca3489c5f852029c3a417396885
lrx0507 <Lurenxi2004@126.com>
2024-03-11 22:03:16 +0800

    Exercise00updated

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -2,6 +2,12 @@ package oy.tol.tra;
 
 
 public class Algorithms {
+   public static <T > void swap(T[] array, int x, int y) {
+
+      T tmp = array[x];
+      array[x] = array[y];
+      array[y] = tmp;
+   }
     public static <T extends Comparable<T>> void sort(T [] array) {
     
       int i = array.length-1;
@@ -12,9 +18,7 @@ public class Algorithms {
          {
             if(array[k].compareTo(array[k+1]) > 0 )
             {
-               T tmp = array[k];
-               array[k] = array[k+1];
-               array[k+1] = tmp;
+               swap(array, k, k+1);
             }
          }
       }
@@ -26,10 +30,8 @@ public class Algorithms {
        
             int i = 0;
             while (i < array.length / 2) {
-                T temp = array[i];
-                array[i] = array[array.length - i - 1];
-                array[array.length - i - 1] = temp;
-                i++;
+               swap(array, i, array.length-i-1);
+               i++;
             }
         
     }
--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -14,31 +14,10 @@ public class Grades {
    
    public void reverse() {
      
-     /*  int i = 0;
-      while (i < grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }*/
      Algorithms.reverse(grades);
    }
 
    public void sort() {
-      /*int i = grades.length-1;
-      
-      for(int j = i; j > 0; --j)
-      {
-         for(int k = 0; k <= j - 1; ++k)
-         {
-            if(grades[k] > grades[k+1])
-            {
-               int tmp = grades[k];
-               grades[k] = grades[k+1];
-               grades[k+1] = tmp;
-            }
-         }
-      }*/
       Algorithms.sort(grades);
    }
 
